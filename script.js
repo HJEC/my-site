0917aa6b1cf4f0739b37aa71a89533bfb1fcc013
Henry J. E. Crookes <52827871+HJEC@users.noreply.github.com>
2020-05-04 15:35:48 +0200

    Embedding the svg using a jquery insert to div from js file

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 window.onload = function() {
+  // Seperate the header letters into individual spans. (cleaner markup)
   var target = $("#target");
   target.html(
     target.text().replace(/./g, `<span class="hover-letter">$&</span>`)
   );
+
   fetch("https://ranmoji.herokuapp.com/emojis/api/v.1.0/")
     .then(response => response.json())
     .then(data => {
