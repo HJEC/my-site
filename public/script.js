3b42a045cd6938b64d8a6726c8e7ab0751407008
pizza <HJEC92@gmail.com>
2020-05-28 12:18:21 +0200

    hover effect on project tiles is working. added more projects

--- public/script.js
+++ public/script.js
@@ -5,20 +5,15 @@ window.onload = function() {
     target.text().replace(/./g, `<span class="hover-letter">$&</span>`)
   );
 
-  $(".project-wrapper").hover(
-    function() {
-      $(".demo-jpg").toggle();
-      $(".demo-gif").toggle();
-      $(this)
-        .find(".project-text")
-        .addClass("text-slide");
-    },
-    function() {
-      $(this)
-        .find(".project-text")
-        .removeClass("text-slide");
-    }
-  );
+  /* Swap out the static images for gifs for the project tiles */
+  $(".project-wrapper").hover(function() {
+    $(this)
+      .find(".demo-jpg")
+      .toggle();
+    $(this)
+      .find(".demo-gif")
+      .toggle();
+  });
 
   fetch("https://ranmoji.herokuapp.com/emojis/api/v.1.0/")
     .then(response => response.json())
